daf35ca12f4a22c308a5e2a2532467c7960098e9
goldhadis <goldhadis@gmail.com>
2018-02-11 23:57:16 +0800

    Day02

---
+++ Day01/js/index.js
@@ -0,0 +1,9 @@
+$(function () {
+	$(".small").click(function() {
+		$(this).css("background-color","yellow");
+	});
+	// $(".btn3").click(function() {
+	// 	$(".small").css("background-color","red");
+	// 	/* Act on the event */
+	// });
+});
